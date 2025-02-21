9d59460067a8e03ca4abbdba2f1da330bb50c279
tahsina-azam <tahsina.sheeva@gmail.com>
2025-02-21 19:37:26 +0600

    doc: update readme

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,33 +1,32 @@
-"use client"
+'use client';
 
 import OnboardingView from '@/sections/onboarding/views/onboarding-view';
 import ProtectedRoute from '@/components/protected-route';
 import { useAuth } from 'context/authContext';
 import { useRouter } from 'next/navigation';
-import { useState,useEffect } from 'react';
-
+import { useState, useEffect } from 'react';
 
 export default function Page() {
-   const { token, logout } = useAuth();
-    const router = useRouter();
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      if (!token) {
-        router.replace('/login'); // Redirect to login if not authenticated
-      } else {
-        setLoading(false);
-      }
-    }, [token, router]);
-  
-    if (loading) {
-      return (
-        <div className="h-screen flex justify-center items-center">
-          Loading...
-        </div>
-      );
+  const { token, logout } = useAuth();
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!token) {
+      router.replace('/login'); // Redirect to login if not authenticated
+    } else {
+      setLoading(false);
     }
-  
+  }, [token, router]);
+
+  if (loading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        Loading...
+      </div>
+    );
+  }
+
   return (
     <ProtectedRoute>
       <OnboardingView />
